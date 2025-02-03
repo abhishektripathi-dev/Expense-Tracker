61ab57c1d1d053ff62f6f1ddfc2c4639e4a740cc
abhishek8182 <abhishektripathi3196@gmail.com>
2025-02-03 07:10:17 +0530

    .env file updated

--- Back-End/utils/database.js
+++ Back-End/utils/database.js
@@ -1,8 +1,16 @@
+const path = require("path");
 const { Sequelize } = require("sequelize");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
-const sequelize = new Sequelize("expense_tracker", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME?.trim(),
+  process.env.DB_USER?.trim(),
+  process.env.DB_PASSWORD?.trim(),
+  {
+    host: process.env.DB_HOST?.trim(),
+    dialect: process.env.DB_DIALECT?.trim() || "mysql", // Default to "mysql"
+  }
+);
 
 module.exports = sequelize;
