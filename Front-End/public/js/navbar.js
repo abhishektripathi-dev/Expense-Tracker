1daf1387ed2d9389b776dfa7696d74b045c878ae
abhishek8182 <abhishektripathi3196@gmail.com>
2025-02-04 11:19:43 +0530

    update navbar

--- Front-End/public/js/navbar.js
+++ Front-End/public/js/navbar.js
@@ -1,4 +1,15 @@
-document.getElementById('hamburger').addEventListener('click', () => {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('active');
-});
+// document.getElementById('hamburger').addEventListener('click', () => {
+//   const navLinks = document.getElementById('nav-links');
+//   navLinks.classList.toggle('active');
+// });
+
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "350px";
+    } else {
+        menuList.style.maxHeight = "0px";
+    }
+}
